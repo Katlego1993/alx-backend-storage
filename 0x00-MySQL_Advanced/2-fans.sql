9d5d5756c4151ee967246a47e681e564282ce0b2
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2024-03-21 15:10:17 +0000

    all

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Rank country origins of bands by number of fans
-SELECT origin, SUM(nb_fans) as total_fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,13 +1,10 @@
-DELIMITER //
-
-CREATE TRIGGER reset_valid_email
-BEFORE UPDATE ON users
+-- Write a SQL script that creates a trigger that resets the attribute valid_email
+DELIMITER $$ ;
+CREATE TRIGGER resets_valid_email BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email <> NEW.email THEN
-        SET NEW.valid_email = 0; -- Assuming 0 represents an invalid email
-    END IF;
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
 END;
-//
-
 DELIMITER ;
